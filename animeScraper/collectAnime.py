2a88073d9101a32a48f5d27462ca6b31e0796cb7
kiamco <kiamcoo@gmail.com>
2020-01-22 11:59:35 -0800

    img

--- animeScraper/collectAnime.py
+++ animeScraper/collectAnime.py
@@ -4,17 +4,17 @@ import re
 from selenium.webdriver.chrome.options import Options
 
 
-def initBrowser():
-    chromeDriver="C:/Program Files/webdriver/chromedriver"
+def init_browser():
+    chromeDriver = "C:/Program Files/webdriver/chromedriver"
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    browser = webdriver.Chrome(chromeDriver, chrome_options=options)
+    options.add_argument(' -- headless')
+    options.add_argument(' -- disable-gpu')
+    browser = webdriver.Chrome(chromeDriver)
 
     return browser
 
-def episodeIterator(anime, browser):
-    parsedName = anime.replace(" ","-")
+def episode_iterator(anime, browser):
+    parsedName = anime.replace(" ", "-")
     ep = 1
 
     try:
@@ -25,26 +25,27 @@ def episodeIterator(anime, browser):
     time.sleep(3)
 
     list = []
-    videoBlock= browser.find_element_by_xpath("//ul[@class='listing items lists']");
+    videoBlock = browser.find_element_by_xpath("//ul[@class = 'listing items lists']");
     list = videoBlock.find_elements_by_class_name('video-block')
     
     epLen = len(list)
     episodes = []
 
-    for x in range(1,epLen + 1):
+    for x in range(1, epLen + 1):
         episodes.append({
-            'title':parsedName,
-            'link': f'http://vidstreaming.io/videos/{parsedName}-episode-{x}',
+            'title':parsedName, 
+            'link': f'http://vidstreaming.io/videos/{parsedName}-episode-{x}', 
             'ep':x
         })
 
         
     return episodes
 
+    
+
 def crawl(anime):
-    browser = initBrowser()
-    targets = episodeIterator(anime, browser)
-    print(targets)
+    browser = init_browser()
+    targets = episode_iterator(anime, browser)
     srcs = []
     for x in targets:
         browser.get(x.get('link'))
@@ -52,19 +53,25 @@ def crawl(anime):
         try:
             title = browser.find_element_by_xpath('/html/body/div[1]/section/div[1]/div[5]/div/div[1]/h1')
             links = browser.find_element_by_xpath('/html/body/div[1]/section/div[1]/div[5]/div/div[1]/div[1]/div[1]/iframe')
+            imgPlaceholder = browser.find_elements_by_xpath("//div[@class = 'video-info-left xh-highlight']//li[1]//a[1]//div[1]//div[1]//img[1]")
+            
+            for x in imgPlaceholder:
+                print("sup",x.get_attribute('src'))
+                
         except:
             print("element not found")
             break 
 
         srcs.append({
-            'title': x.get('title'),
-            'episode': x.get('ep'),
+            'title': x.get('title'), 
+            'episode': x.get('ep'), 
             'link':links.get_attribute("src")
         })
 
         time.sleep(3)
 
     print(srcs)
+
 
     
 
